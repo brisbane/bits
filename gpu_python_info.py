013b02a687bfca338bb03aa5c02518e5ce9fed6b
brisbane <sean.bris@gmail.com>
2025-10-03 10:47:04 +0100

    Update gpu_python_info.py
    support no cuda

--- gpu_python_info.py
+++ gpu_python_info.py
@@ -3,7 +3,10 @@ from tensorflow.python.client import device_lib
 import tensorflow as tf
 
 sys_details = tf.sysconfig.get_build_info()
-cuda_version = sys_details["cuda_version"]
+cuda_version="No cuda"
+if "is_cuda_build" in  sys_details and sys_details['is_cuda_build']:
+   cuda_version = sys_details["cuda_version"]
+
 
 print(device_lib.list_local_devices())
-print(cuda_version)
+print("cuda version" , cuda_version)
